785f5acd849a4a1cde851242a70d84582679cc4a
Gulyashenko <igor89037078337@yandex.ru>
2025-02-23 16:43:07 +0300

    решение первого задания

---
+++ Person.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <string>
+#include <iostream>
+using namespace std;
+
+class  Person
+{
+private:
+	string lastname;
+	string name;
+	string patronymic;
+public:
+	Person();
+	Person(const string& ln, const string& n, const string p = "");
+	void show(const string& lastname, const string& name); // ln + n
+	void showFormal(const string& lastname, const string& name, const string& patronymic); //ln + n + p 
+};
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include "Person.h"
+
+int main(void)
+{
+	Person fio;
+	fio.show("Gulin", "Igor");
+	fio.showFormal("Gulin", "Igor", "Alekseewich");
+}
